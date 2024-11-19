21f09fa13e978920249480c603a6618318b8beb3
ijonathone <ijonathone@gmail.com>
2024-11-18 22:27:28 -0500

    Creacion login, logout

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -36,40 +36,47 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const { email, password } = req.body;
     const userFound = await User.findOne({ email });
 
     if (!userFound)
-      return res.status(400).json({
-        message: ["The email does not exist"],
-      });
+      return res.status(400).json({message: "User not found"});
 
     const isMatch = await bcrypt.compare(password, userFound.password);
-    if (!isMatch) {
-      return res.status(400).json({
-        message: ["The password is incorrect"],
-      });
-    }
+    if (!isMatch) 
+      return res.status(400).json({ message: "The password is incorrect"});
+    
 
-    const token = await createAccessToken({
-      id: userFound._id,
-      userName: userFound.userName,
-    });
-
-    res.cookie("token", token, {
-      httpOnly: process.env.NODE_ENV !== "development",
-      secure: true,
-      sameSite: "none",
-    });
-
+    const token = await createAccessToken({id: userFound._id});
+    res.cookie("token", token)
     res.json({
       id: userFound._id,
-      username: userFound.userName,
+      userName: userFound.userName,
       email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-};
+}
 
+export const logout =  (req, res) => {
+  res.cookie("token", "", {expires: new Date(0)})
+
+return res.sendStatus(200);
+}
+
+
+export const profile = async (req, res) =>{
+  const userFound = await User.findById(req.user.id)
+  if(!userFound) return res.status(400).json({message: "User not found"})
+    return res.json({
+  id: userFound._id,
+  userName: userFound.userName,
+email: userFound.email,
+createdAt: userFound.createdAt,
+updatedAt: userFound.updatedAt,
+})
+}
